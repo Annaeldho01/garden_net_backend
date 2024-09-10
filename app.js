8660529f04af88a50345a2c699c251cfdd5638d5
Annaeldho01 <annaeldho001@gmail.com>
2024-09-10 18:54:40 +0530

    signin completed

--- app.js
+++ app.js
@@ -13,7 +13,35 @@ app.use(cors())
 
 mongoose.connect("mongodb+srv://anna:anna@cluster0.ld2gi.mongodb.net/gardenAppDb?retryWrites=true&w=majority&appName=Cluster0")
 
+//signin
+app.post("/signIn",async(req,res)=>{
+    let input=req.body
+    let result=userModel.find({email:req.body.email}).then(
+        (items)=>{
+            if(items.length>0)
+            {
+                const passwordvalidator=bcrypt.compareSync(req.body.password,items[0].password)
+               if (passwordvalidator) {
+                    jwt.sign({email:req.body.email},"gardenapp",{expiresIn:"1d"},(error,token)=>{
+                        if (error) {
+                            res.json({"status":" Error","errorMessage":error})
+                        } else {
+                            res.json({"status":"Success","token":token,"userId":items[0]._id})
+                        }
+                    })
+               } else {
+                res.json({"status":"Incorrect password"})
+               }
+            }
+            else{
+                res.json({"status":"Invalid email id "})
+            }
+        }
+    ).catch()
+  })
 
+
+//signup
 app.post("/signup",async(req,res)=>{
     let input=req.body
     let hashedPassword=bcrypt.hashSync(req.body.password,10)
